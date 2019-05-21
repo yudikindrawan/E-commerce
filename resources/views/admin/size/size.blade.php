7347651fe9bf6333aa2cefcf730e50c003efbbab
Arti Bali Code <yudikidrr@gmail.com>
2019-05-21 16:30:44 +0800

    Indah Permata

--- resources/views/admin/size/size.blade.php
+++ resources/views/admin/size/size.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>ARTI BALI</title>
+  <title>ARTI BALI Manufaktur</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
